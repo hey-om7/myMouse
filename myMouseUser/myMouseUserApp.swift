dea6cc7e02a713940b8846d1d650ae9e1d9732cf
hey-om7 <om.ambarkar@gmail.com>
2025-11-19 16:51:09 +0530

    Initial Commit

---
+++ myMouseUser/myMouseUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMouseUserApp.swift
+//  myMouseUser
+//
+//  Created by Om Ambarkar on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct myMouseUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
